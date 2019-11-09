38ea782d9026fa41ccee44f1e09ff3f235f270a9
lucysyt <2910760609@qq.com>
2019-11-09 13:20:58 +0800

    ss

---
+++ src/components/AboutUs/index.vue
@@ -0,0 +1,204 @@
+<template>
+	<div class="container">
+      <div class="container-fuild about-us-box">
+      	<div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-6" style="padding-top: 25px;">
+          	<h3>Welcome to Doing Machine</h3>
+          	<div class="about-us">
+              <p>&nbsp;</p>
+          		<p>Changzhou Doing Machine Co., Ltd has been manufacturing, engineering and exporting various kinds of industrial dryers,mixers, granulators and impact mills since 1995, for thousands of satisfied customers.</p>
+          		<p>Attention to detail and real production data is critical in the manufacturing of industrial machinery. An unique design for specific material is always made based on real production experiences or trial data.</p>
+          		<p>Doing Machine has experienced technicians who has been working in this field for more than 30 years as well as professional pilot plant.Free trial and material study are available for our customers.This is also true after the sale . Doing Machine has skilled and experienced after sales service team who can reach customers in short notice for providing professional installation, commissioning and training services.</p>
+                <p>DoingMachine is always your reliable machinery supplier.</p>
+          	</div>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-6 about-img">
+          	<img class="lazy" src="../../assets/img/gongsi.jpg" alt="gongsi" title="gongsi" style="visibility: visible;">
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-12">
+          	<div class="about-i">
+          		<img class="lazy"src="../../assets/img/ABOUT-US.png" title="ABOUT US" style="visibility: visible;">
+          	</div>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-3">
+            <div class="about-p">
+              <img class="lazy" src="../../assets/img/tu1.jpg" alt="tu3" title="tu3" style="visibility: visible;">
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-3">
+            <div class="about-p">
+              <img class="lazy" src="../../assets/img/tu2.jpg" alt="tu3" title="tu3" style="visibility: visible;">
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-3">
+            <div class="about-p">
+              <img class="lazy" src="../../assets/img/tu3.jpg" alt="tu3" title="tu3" style="visibility: visible;">
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-3">
+            <div class="about-p">
+              <img class="lazy" src="../../assets/img/tu4.jpg" alt="tu3" title="tu3" style="visibility: visible;">
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="about-f">
+              <h2>WHAT WE DO</h2>
+              <hr/>
+              <p>DOING MACHINE is specialized in design, manufacturing, engineering and exporting of industrial dryers, impact mills, powder mixers, wet granulators and conveying equipements.</p>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-4 col-md-4">
+            <div class="about-n">
+              <div class="e-row">
+                <div class="col-xs-2 col-sm-2 col-md-2 an-1">1</div>
+                <div class="col-xs-10 col-sm-10 col-md-10 an-2">
+                  <p>ADVANTAGE<br/>TEST CENTER</p>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12 an-3">
+                  <p>We can test your product for free to determine the optimal process for final design. </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-4 col-md-4">
+            <div class="about-n">
+              <div class="e-row">
+                <div class="col-xs-2 col-sm-2 col-md-3 an-1">2</div>
+                <div class="col-xs-10 col-sm-10 col-md-9 an-2">
+                  <p>ADVANTAGE<br/>ON-SITE TRAINING</p>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12 an-3">
+                  <p>On-site advice, engineering and traning to help you to optimise your process.  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-4 col-md-4">
+            <div class="about-n">
+              <div class="e-row">
+                <div class="col-xs-2 col-sm-2 col-md-3 an-1">3</div>
+                <div class="col-xs-10 col-sm-10 col-md-9 an-2">
+                  <p>ADVANTAGE<br/>SPARE PARTS</p>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12 an-3">
+                  <p>Avoid the hassle of souring spare parts with our extensive stock and swift delivery.  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="about-f">
+              <h2>VISITING CUSTOMERS</h2>
+              <hr/>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-12 col-md-4">
+            <div class="about-p">
+              <img class="lazy" src="../../assets/img/0.jpg" alt="tu3" title="tu3" style="visibility: visible;">
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-4">
+            <div class="about-p">
+              <img class="lazy" src="../../assets/img/1.jpg" alt="tu3" title="tu3" style="visibility: visible;">
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-4">
+            <div class="about-p">
+              <img class="lazy" src="../../assets/img/2.jpg" alt="tu3" title="tu3" style="visibility: visible;">
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-12">
+            <div style="height: 100px"></div>
+          </div>
+
+         </div>
+        </div>
+	</div>
+</template>
+<script>
+export default {
+    name: "AboutUs",
+    data() {
+      return {};
+    },
+    mounted() {
+      
+    }
+  };
+</script>
+ <style lang="scss" scoped> 
+.about-us {
+	font-family: Open Sans;
+    font-size: 14px;
+    color: #545454;
+    letter-spacing: 0;
+    line-height: 1.7em;
+    text-align: left;
+}
+.about-img {
+  padding-top:45px;
+  img{
+  width: 100%;
+}
+  } 
+.about-us-box {
+  font-family: Open Sans;
+	.about-i {
+    padding: 50px 0;
+    img {
+      max-width: 100%;
+    }
+  }
+  .about-p {
+    padding-bottom:10px;
+    text-align: center;
+    img {
+      display: inline-block;
+      max-width: 100%;
+    }
+  }
+  .about-f {
+    padding:0 20%;
+    padding-top:30px;
+    padding-bottom:50px;
+    text-align: center;
+    line-height: 1.7em;
+    hr {
+      width: 70%;
+    }
+  }
+  .about-n {
+    .an-1 {
+      font-size: 120px;
+      opacity: .15;
+      font-weight: 400;
+      color: red;
+    }
+    .an-2 {
+      height: 130px;
+    position:relative;
+      p {
+        font-size: 18px;
+        line-height: 24px;
+        color: red;
+        font-weight: bold;
+        position:absolute;
+       bottom:0px;
+       padding:0px;
+       margin:0px;
+       letter-spacing:0px;
+      }
+    }
+    .an-3 {
+      font-size: 16px;
+      color: #888;
+      line-height: 27px;
+    }
+  }
+} 
+</style>
+
